b38e55b064e6063ab94f107d68dd5a4a8c811da2
A.Irwin Putra Pangesti <aipp.project@gmail.com>
2024-11-15 16:01:44 +0700

    optimalization

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,12 +1,19 @@
 // src/public/views/pages/home.js
-import RestaurantSource from '../../data/restaurant-source';
-import CONFIG from '../../globals/config';
+import RestaurantSource from "../../data/restaurant-source";
+import CONFIG from "../../globals/config";
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
 
 const Home = {
   async render() {
     return `
       <section class="hero">
-        <img src="../images/heros/hero-image_1.jpg" alt="Delicious food served" class="hero__image">
+        <img 
+          data-src="../images/heros/hero-image_1-1200.webp" 
+          alt="Delicious food served" 
+          class="hero__image lazyload" 
+          width="1200" 
+          height="600">
         <h2 class="hero__title">Welcome to Our Restaurant Catalogue</h2>
       </section>
 
@@ -19,13 +26,18 @@ const Home = {
 
   async afterRender() {
     const restaurants = await RestaurantSource.list();
-    const restaurantListContainer = document.getElementById('restaurant-list');
+    const restaurantListContainer = document.getElementById("restaurant-list");
 
     restaurants.forEach((restaurant) => {
       restaurantListContainer.innerHTML += `
         <a href="#/detail/${restaurant.id}" class="restaurant-item">
           <article>
-            <img class="restaurant-item__image" src="${CONFIG.BASE_IMAGE_URL.SMALL}${restaurant.pictureId}" alt="${restaurant.name}">
+            <img 
+              class="restaurant-item__image lazyload" 
+              data-src="${CONFIG.BASE_IMAGE_URL.SMALL}${restaurant.pictureId}" 
+              alt="${restaurant.name}" 
+              width="300" 
+              height="200">
             <div class="restaurant-item__content">
               <h3 class="restaurant-item__name">${restaurant.name}</h3>
               <p class="restaurant-item__city">City: ${restaurant.city}</p>
--- webpack.common.js
+++ webpack.common.js
@@ -1,51 +1,89 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
+const ImageminMozjpeg = require("imagemin-mozjpeg");
+const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
+    main: path.resolve(__dirname, "src/scripts/index.js"),
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: "single",
+  },
   plugins: [
     new CleanWebpackPlugin(),
-
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/templates/index.html"),
     }),
-
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, "src/public"),
+          to: path.resolve(__dirname, "dist"),
         },
       ],
     }),
-
-    // Inject Workbox service worker with custom caching logic
     new InjectManifest({
-      swSrc: path.resolve(__dirname, 'src/scripts/service-worker.js'),
-      swDest: 'service-worker.js',
+      swSrc: path.resolve(__dirname, "src/scripts/service-worker.js"),
+      swDest: "service-worker.js",
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+      ],
+    }),
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 50,
+          },
+        },
+      ],
+      overrideExtension: true,
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: true,
     }),
   ],
 };
